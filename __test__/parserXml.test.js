959202907b5a79fd45ebcda78480a95d897cdf0f
Dmitry Kabanov <dmitrykabanov@Mini-Dmitrij.jetstyle.int>
2018-09-06 10:48:58 +0500

    fixed the tests

--- __test__/parserXml.test.js
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-import parseXml from '../src/parser';
-
-test('Check parsing XML', () => {
-  const feedStr = fs.readFileSync('./__fixtures__/feed', 'utf8');
-  const actual = parseXml(feedStr);
-  const expected = {};
-  expect(actual).toBe(expected);
-});
---
+++ __tests__/parserXml.test.js
@@ -0,0 +1,19 @@
+import fs from 'fs';
+import parseXml from '../src/parser';
+
+test('Check parsing XML', () => {
+  const feedStr = fs.readFileSync('__tests__/__fixtures__/feed', 'utf8');
+  const actual = parseXml(feedStr);
+  const expected = {
+    description: 'This is a constantly updating lorem ipsum feed',
+    items: [
+      {
+        description: 'Ullamco est consequat voluptate laboris id qui voluptate amet laborum Lorem velit irure dolor mollit.',
+        link: 'http://example.com/test/1536204180',
+        title: 'Lorem ipsum 2018-09-06T03:23:00+00:00',
+      },
+    ],
+    title: 'Lorem ipsum feed for an interval of 1 minutes',
+  };
+  expect(actual).toEqual(expected);
+});
