30bf99cacf0deeef4ef748ab0afa04fba67e7bbb
Dmitry Kabanov <dkabanov6@gmail.com>
2018-09-09 10:04:14 +0500

    update test

--- __tests__/parserXml.test.js
+++ __tests__/parserXml.test.js
@@ -11,9 +11,11 @@ test('Check parsing XML', () => {
         description: 'Ullamco est consequat voluptate laboris id qui voluptate amet laborum Lorem velit irure dolor mollit.',
         id: 'newsId_1',
         link: 'http://example.com/test/1536204180',
+        pubDate: 'Thu, 06 Sep 2018 03:23:00 GMT',
         title: 'Lorem ipsum 2018-09-06T03:23:00+00:00',
       },
     ],
+    link: 'http://example.com/',
     title: 'Lorem ipsum feed for an interval of 1 minutes',
   };
   expect(actual).toEqual(expected);
